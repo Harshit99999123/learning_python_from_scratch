fa62e3dfb5e98774efd2fa740a27ae325ac913bf
Harshit <harshit@Harshits-MacBook-Pro.local>
2024-12-05 21:56:19 +0530

    Initial commit

---
+++ day1/project1_band_name_generator.py
@@ -0,0 +1,4 @@
+city_name = input("What is the name of the city you grew up in? \n")
+pet_name = input("What is your pet's name? \n")
+band_name = city_name + " " + pet_name
+print("Your band name could be " + band_name)
